2849ab5d72d7d41a7f35079f59835a6e25604d32
Jou-An Chen <jchen8509@gmail.com>
2023-10-12 11:22:19 -0500

    Sync

--- Hpoi_scraping.py
+++ Hpoi_scraping.py
@@ -1,37 +1,60 @@
-import requests
+from enum import Enum
 from bs4 import BeautifulSoup, Tag, ResultSet
 from dataclasses import dataclass
+from hpoi_translation import Process
+import aiohttp
+import asyncio
 
 URL = "https://www.hpoi.net"
-wait_time_seconds:float = 60 * 5
+wait_time_seconds: float = 60 * 5
+BATCH_SIZE = 20
+
+
+class STATUS(Enum):
+    NEW_ANNOUNCEMENT = "New Announcement"
+    IMG_UPDATE = "Image Update"
+    INFO_UPDATE = "Info Update"
+    PO_OPENED = "Pre-Orders Opened"
+    RELEASE_DATE = "Release Date"
+    DELAYED = "Delayed"
+    RE_RELEASE = "Re-Release"
+
+
 # Chinese translations
-translations = {
-    "Name": '名称',
-    "Origin": '作品',
-    "Character": '角色',
-    "Manufacturer": '制作',
-    "Illustrator": '原画',
-    "Release Date": '发售日',
-    "Price": '定价',
-    "Material": '材质',
-    "Scale": '比例',
-    "Dimension": '尺寸'
+TRANSLATIONS = {
+    "Name": "名称",
+    "Origin": "作品",
+    "Character": "角色",
+    "Manufacturer": "制作",
+    "Illustrator": "原画",
+    "Release Date": ["发售", "发售日"],
+    "Price": "定价",
+    "Material": "材质",
+    "Scale": "比例",
+    "Dimension": "尺寸",
+    # --OUTER PAGE--
+    "制作决定": STATUS.NEW_ANNOUNCEMENT,
+    "官图更新": STATUS.IMG_UPDATE,
+    "": STATUS.INFO_UPDATE,
+    "预定时间": STATUS.PO_OPENED,
+    "出荷时间": STATUS.RELEASE_DATE,
+    "出荷延期": STATUS.DELAYED,
+    "再版确定": STATUS.RE_RELEASE,
 }
 # number of cards to poll at once
-batch_size = 5
+
 
 # TODO: add consts for selectors
 @dataclass
 class hpoiCard:
     # --OUTER PAGE--
-    # update title
+    # card title
     title: str
     # update status
-    status: str
-    # description for update status
-    status_description: str
+    status: STATUS
     # full link to item page
     link: str
+    # image
     img_src: str
 
     # --INNER PAGE--
@@ -46,77 +69,143 @@ class hpoiCard:
     scale: str
     dimension: str
 
-    ## def __str__(self):
-    ##    return 'TODO'
 
-def tag_to_card(tag: Tag) -> hpoiCard: 
-    # --OUTER PAGE--
-    image:Tag = tag.find("div", class_="left-leioan").find("a")
-    info:Tag = tag.find("div", class_="right-leioan")
+async def tag_to_card(tag: Tag, session) -> hpoiCard:
+    async with aiohttp.ClientSession() as session:
+        # --OUTER PAGE--
+        image: Tag = tag.find("div", class_="left-leioan").find("a")
+        info: Tag = tag.find("div", class_="right-leioan")
 
-    title:str = info.find_all("div")[4].text
-    status:str = info.find("div").find("span").text
-    status_description:str = info.find_all("div")[3].text
-    link:str = URL + "/" + image.get("href")
-    img_src:str = image.find("img").get("src")
+        title: str = info.find_all("div")[4].text
+        status: STATUS = TRANSLATIONS.get(info.find("div").find("span").text)
+        # print(status)
+        link: str = URL + "/" + image.get("href")
+        img_src: str = image.find("img").get("src")
 
-    # --INNER PAGE--m
-    page = requests.get(link)
-    inner_soup = BeautifulSoup(page.content, "html.parser")
+        # --INNER PAGE--
+        res = await session.get(link)
+        page = await res.read()
+        inner_soup = BeautifulSoup(page, "html.parser")
 
-    inner_info:Tag = inner_soup.find("div", class_= "infoList-box")
-    
-    name = getItem(inner_info, translations.get("Name"))
-    origin = getItem(inner_info, translations.get("Origin"))
-    character = getItem(inner_info, translations.get("Character"))
-    manufacturer = getItem(inner_info, translations.get("Manufacturer"))
-    illustrator = getItem(inner_info, translations.get("Illustrator"))
-    release_date = getItem(inner_info, translations.get("Release Date"))
-    price = getItem(inner_info, translations.get("Price"))
-    material = getItem(inner_info, translations.get("Material"))
-    scale = getItem(inner_info, translations.get("Scale"))
-    dimension = getItem(inner_info, translations.get("Dimension"))
+        inner_info: Tag = inner_soup.find("div", class_="infoList-box")
 
-    return hpoiCard(title, status, status_description, link, img_src, 
-                    name, origin, character, manufacturer, illustrator,
-                    release_date, price, material, scale, dimension)
-                    
+        name = getItem(inner_info, TRANSLATIONS.get("Name"))
+        # what does it take in = type
+        # what variable it takes in = variable name
+
+        origin = getItem(inner_info, TRANSLATIONS.get("Origin"))
+        character = getItem(inner_info, TRANSLATIONS.get("Character"))
+        manufacturer = getItem(inner_info, TRANSLATIONS.get("Manufacturer"))
+        illustrator = getItem(inner_info, TRANSLATIONS.get("Illustrator"))
+        release_date = getItem(inner_info, TRANSLATIONS.get("Release Date"))
+        price = getItem(inner_info, TRANSLATIONS.get("Price"))
+        material = getItem(inner_info, TRANSLATIONS.get("Material"))
+        scale = getItem(inner_info, TRANSLATIONS.get("Scale"))
+        dimension = getItem(inner_info, TRANSLATIONS.get("Dimension"))
+        [
+            translatedName,
+            translatedOrigin,
+            translatedCharacter,
+            translatedManufacturer,
+            translatedIllustrator,
+            translatedReleaseDate,
+            translatedPrice,
+            translatedMaterial,
+            translatedScale,
+            translatedDimension,
+        ] = await Process(
+            session,
+            sourceTexts=[
+                name,
+                origin,
+                character,
+                manufacturer,
+                illustrator,
+                release_date,
+                price,
+                material,
+                scale,
+                dimension,
+            ],
+        )
+
+        return hpoiCard(
+            title,
+            status,
+            link,
+            img_src,
+            translatedName,
+            translatedOrigin,
+            translatedCharacter,
+            translatedManufacturer,
+            translatedIllustrator,
+            translatedReleaseDate,
+            translatedPrice,
+            translatedMaterial,
+            translatedScale,
+            translatedDimension,
+        )
+
+
 # Grabs text of info list item safely
 def getItem(tag: Tag, infoList_name: str):
     try:
-        return tag.find("span", string = infoList_name).findNext('p').text
+        return tag.find("span", string=infoList_name).findNext("p").text
     except AttributeError:
-        print(infoList_name + ' not found')
+        # print(infoList_name + " not found")
         return "N/A"
-    
+
+
 titleCache: list[str] = []
 
-def fetchCards() -> list[hpoiCard]:    
-    print('Loading page...')
-    page = requests.get(URL)
-    print('Page loaded!')
-    soup = BeautifulSoup(page.content, "html.parser")
 
-    tags: ResultSet[Tag] = soup.find("div", class_="hpoi-conter-ltsifrato").find_all("div", class_="hpoi-conter-left")
+async def fetchCards() -> list[hpoiCard]:
+    print("Loading page...")
+    async with aiohttp.ClientSession() as session:
+        res = await session.get(URL)
 
-    tags = tags[:batch_size]
+        text = await res.read()
 
-    titles = map(lambda tag: tag.find("div", class_="right-leioan").find_all("div")[4].text, tags)
-    tags_and_titles = zip(tags,titles)
-    tags_and_titles = list(filter(lambda pair: pair[1] not in titleCache, tags_and_titles))
+        if res.status != 200:
+            raise Exception("Website is down! Status code: " + res.status)
 
-    if(len(tags_and_titles) <= 0):
-        print("Found no new cards!")
-        return []
-    print("Found "+str(len(tags_and_titles))+ " new cards!")
+        print("Page loaded!")
+        soup = BeautifulSoup(text, "html.parser")
+        tags: ResultSet[Tag] = soup.find(
+            "div", class_="hpoi-conter-ltsifrato"
+        ).find_all("div", class_="hpoi-conter-left")
+        tags = tags[:BATCH_SIZE]
 
-    cards: list[hpoiCard] = []
-    for tag, title in tags_and_titles:
-        # code to grab card data
-        cards.append(tag_to_card(tag))
-        titleCache.append(title)
-        
+        titles = map(
+            lambda tag: tag.find("div", class_="right-leioan").find_all("div")[4].text,
+            tags,
+        )
+        tags_and_titles = list(zip(tags, titles))
+
+        tags_and_titles = list(
+            filter(lambda pair: pair[1] not in titleCache, tags_and_titles)
+        )
+        if len(tags_and_titles) <= 0:
+            print("Found no new cards!")
+            return []
+        print("Found " + str(len(tags_and_titles)) + " new cards!")
+
+        # TODO: convert to list of tasks
+        cardTasks = []
+        for tag, title in tags_and_titles:
+            # Code to grab card data; make async tag_to_card
+            cardTasks.insert(0, tag_to_card(tag, session))
+
+            titleCache.insert(0, title)
+            # Remove stale entries from cache
+            # if len(titleCache) > BATCH_SIZE:
+                # titleCache.pop(-1)
+        # TODO: call gather here
+        cards = await asyncio.gather(*cardTasks)
     return cards
 
+
 if __name__ == "__main__":
-    fetchCards()
+    # Start the asyncio program
+    # programming languages list
+    asyncio.run(fetchCards())
